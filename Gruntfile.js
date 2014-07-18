9976e0b2ccd273f3f84b5987f4ac079dd709ac74
Ricardo Torres <ricardo@rictorres.com.br>
2014-07-18 11:40:24 +0200

    Remove unnecessary comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
 
 	// Load grunt tasks automatically
